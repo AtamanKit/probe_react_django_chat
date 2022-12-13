02f242ef54e2b41caa4e1b953578b66bab10bd3d
AtamanKit <rodionatamaniuc@gmail.com>
2022-12-14 00:11:46 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import { w3cwebsocket } from 'websocket';
+
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+
+function App() {
+  return (
+    <>
+      <Container component='main' maxWidth='xs'>
+        <Button
+          variant='contained'
+          FullWidth
+        >
+          Proba
+        </Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
